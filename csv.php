6595c4573ec247601e55cce67156b48997aaeb54
William Taylor <willtaylor2@hotmail.com>
2018-03-20 14:32:58 +0000

    fully functional, needs tidying a bit

--- csv.php
+++ csv.php
@@ -1,1 +1,47 @@
 <?php
+
+class Csv {
+  private $data = [];
+  private $csv;
+
+  function __construct($csv) {
+    $this->csv = $this->prepareData($csv);
+  }
+
+  // magic method to get data
+  public function __get($property) {
+    if (property_exists($this, $property)) {
+      return $this->$property;
+    }
+  }
+
+  public function getData() {
+    return $this->$data;
+  }
+
+  private function prepareData($csv) {
+    $data = [];
+    foreach ($csv as $line) {
+      $new_line = str_getcsv($line);
+      $key = $new_line[0];
+      $cost = $new_line[1] * $new_line[2];
+      if(array_key_exists($key, $data)) {
+        $data[$key] += $cost;
+      } else {
+        $data[$key] = $cost;
+      }
+    }
+    $this->data = $data;
+    $this->prepareCsv();
+  }
+
+  private function prepareCsv() {
+    $file = fopen("newData.csv","w");
+    foreach ($this->data as $key => $line) {
+      $newLine = $key . ',' . $line;
+      fputcsv($file, explode(',', $newLine));
+    }
+    fclose($file);
+  }
+
+}
--- index.php
+++ index.php
@@ -3,23 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>Claromentis test</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <table>
-      <?php foreach($data as $k => $line): ?>
-        <tr>
-          <td><?= $k ?></td>
-          <td><?= $line ?></td>
-        </tr>
-      <?php endforeach ?>
-    </table>
-    <?php if(!empty($data)): ?>
-      Download this form as CSV - <a href="#">click here</a>
-    <?php endif ?>
-    <form action="submit.php" method="post">
-      Upload a new CSV file
-      <input type="file" name="file" accept=".csv"/>
-      <input type="submit" name="" value="submit">
-    </form>
+    <div class="body-container  d-block">
+      <?php if(!empty($data['table'])): ?>
+      <table class="table  table-striped">
+        <thead>
+          <tr>
+            <td><strong>Category</strong></td>
+            <td><strong>Cost</strong></td>
+          </tr>
+        </thead>
+        <?php foreach($data['table'] as $category => $cost): ?>
+          <tr>
+            <td><?= $category ?></td>
+            <td>£<?= $cost ?></td>
+          </tr>
+        <?php endforeach ?>
+      </table>
+      <form method="get" action="newData.csv">
+         Download this form as CSV - <button type="submit">Download!</button>
+      </form>
+      <?php endif ?>
+      <form action="submit.php" method="post">
+        <label for="file">Upload a new CSV file</label>
+        <input type="file" name="file" accept=".csv" id="file" class="d-block"/>
+        <input type="submit" name="" value="Submit">
+      </form>
+    </div>
   </body>
 </html>
